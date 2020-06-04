26e2637e8ae95688e1a3d964af5a9dc9be0e88e0
Nai <bapdola304@gmail.com>
2020-06-04 23:54:40 +0700

    part 3 toeic

--- botToeic.py
+++ botToeic.py
@@ -10,6 +10,7 @@ import numpy as np
 #define path
 toeicPart1Path = './data/toeicPart1.json'
 toeicPart2path = './data/toeicPart2.json'
+toeicPart3path = './data/toeicPart3.json'
 tokenPath = './config/token'
 accountPath ='./config/account.json'
 
@@ -27,6 +28,9 @@ part1Data= open(toeicPart1Path, encoding='utf-8')
 part1FormatJson = json.load(part1Data)
 part2Data= open(toeicPart2path, encoding='utf-8')
 part2FormatJson = json.load(part2Data)
+part3Data= open(toeicPart3path, encoding='utf-8')
+part3FormatJson = json.load(part3Data)
+
 da = ["A", "B", "C", "D"]
 dad = '1'
 
@@ -98,5 +102,49 @@ class SkypePing(SkypeEventLoop):
                     event.msg.chat.sendMsg( da[i-1] +  '. ' + list_anwser[i].get('content'))
             except:
               event.msg.chat.sendMsg('syntax: p2 questionId')
+          if "toeic part3" in event.msg.content.lower() or "toeic part 3" in event.msg.content.lower() or "part 3" in event.msg.content.lower() or "part3" in event.msg.content.lower():
+            part3Item = random.choice(part3FormatJson)
+            audio_url = part3Item.get('audio')
+            question_id = part3Item.get('question_id')
+            audio_data = requests.get(audio_url).content
+            with open('part3.mp3', 'wb') as handler:
+                handler.write(audio_data) # write do dia
+            event.msg.chat.sendMsg('Part 3: Question ' + question_id)
+            event.msg.chat.sendFile(open("part3.mp3", "rb"), "part3.mp3", False, True)
+            part3Question = part3Item.get('answers')
+            part3List = np.array(part3Question)
+            for i in range(len(part3List)):
+              for j in range(len(part3List[i])):
+                if j == 0:
+                  event.msg.chat.sendMsg('Câu ' + str(i + 1)+ ': '+ part3List[i][j].get('content'))
+                else:
+                  event.msg.chat.sendMsg('       ' + da[j-1] + '. ' + part3List[i][j].get('content'))
+            os.remove("part3.mp3") #remove file
+          if "p3" in event.msg.content.lower():
+            question_id = event.msg.content.lower().split(" ")[1]
+            try:
+              arr = np.array(part3FormatJson)
+              arrTemp3 = []
+              for element in arr:
+                if element.get('question_id') == question_id:
+                  arrTemp3.append(element)
+                  break
+              answerList = arrTemp3[0].get('answers')
+              conversationList = arrTemp3[0].get('conversation')
+              event.msg.chat.sendMsg('Câu ' + question_id + '. Đoạn hội thoại:')
+              audio_url = arrTemp3[0].get('audio')
+              audio_data = requests.get(audio_url).content
+              with open('part3.mp3', 'wb') as handler:
+                  handler.write(audio_data) # write do dia
+              event.msg.chat.sendFile(open("part3.mp3", "rb"), "part3.mp3", False, True)
+              for con in conversationList:
+                  event.msg.chat.sendMsg(con)
+              for i in range(len(answerList)):
+                for j in range(len(answerList[i])):
+                    if answerList[i][j].get('correct') == '1':
+                      event.msg.chat.sendMsg('Câu ' + str(i + 1)+ ' - '+ da[j - 1])
+              os.remove("part3.mp3") #remove file
+            except:
+              event.msg.chat.sendMsg('syntax: p3 questionId')
 sk = SkypePing(tokenFile=tokenPath, autoAck=True)
 sk.loop()
